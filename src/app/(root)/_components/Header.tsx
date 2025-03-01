e6dc3b6d1890adb61d43e2c808d88cd0c429367c
Sourish Bose <sourish.cse121120@bppimt.ac.in>
2025-03-01 17:41:56 +0530

    fixed has access

--- src/app/(root)/_components/Header.tsx
+++ src/app/(root)/_components/Header.tsx
@@ -75,7 +75,7 @@ async function Header() {
           <div className="flex items-center gap-4">
             <div className="flex items-center gap-3">
               <ThemeSelector />
-              <LanguageSelector hasAccess={Boolean(convexUser?.isPro)} />
+              <LanguageSelector />
             </div>
   
             {!convexUser?.isPro && (
